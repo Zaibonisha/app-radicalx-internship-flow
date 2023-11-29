63afe1e83056021ba459f553ae32d225b30db459
Zaibonisha <zsmohamed94@gmail.com>
2023-11-29 12:19:52 +0200

    Changes

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -17,7 +17,7 @@ function LoginPage({ onLogin }) {
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    if (username === 'Nisha' && password === '12345') {
+    if (username === 'Zaibonisha' && password === '12345') {
       onLogin(username);
       setUsername('');
       setPassword('');
